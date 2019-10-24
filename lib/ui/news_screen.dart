471aa16c309b04a52cc52e592368aeab5a39edd2
HARSH JAIN <jainharsh270@gmail.com>
2019-10-24 20:17:51 +0530

    news incoming

--- lib/ui/news_screen.dart
+++ lib/ui/news_screen.dart
@@ -21,27 +21,40 @@ class _NewsScreenState extends State<NewsScreen> {
   String title2 = "";
   String description2 = "";
   String urlToImage2 = "";
-  List<String> titles = [" ", " ", " ", " ", " "];
-  List<String> descriptions = [" ", " ", " ", " ", " "];
-  List<String> urlToImages = [" ", " ", " ", " ", " "];
+  // List<String> titles = new List(20);
+  List<String> titles = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "];
+  List<String> descriptions = [
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " "
+  ];
+  List<String> urlToImages = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "];
+
   _NewsScreenState(this.topicName);
 
-  void showData() async {
-    news = await getNews(util.apiId, topicName.toLowerCase());
-    articles = news["articles"];
-    title = articles[0]["title"];
-    description = articles[0]["description"];
-    urlToImage = articles[0]["urlToImage"];
-    title2 = articles[1]["title"];
-    description2 = articles[1]["description"];
-    urlToImage2 = articles[1]["urlToImage"];
-    // titles.add(articles[0]["title"]);
-  }
+  // void showData() async {
+  //   news = await getNews(util.apiId, topicName.toLowerCase());
+  //   articles = news["articles"];
+  //   title = articles[0]["title"];
+  //   description = articles[0]["description"];
+  //   urlToImage = articles[0]["urlToImage"];
+  //   title2 = articles[1]["title"];
+  //   description2 = articles[1]["description"];
+  //   urlToImage2 = articles[1]["urlToImage"];
+  //   // titles.add(articles[0]["title"]);
+  // }
 
   @override
   void initState() {
     super.initState();
-    showData();
+    // showData();
   }
 
   @override
@@ -68,19 +81,18 @@ class _NewsScreenState extends State<NewsScreen> {
                 // articles = news['articles'][0]['title'];
                 // titles.add(news['articles'][0]['title']);
                 articles = news["articles"];
-                title = articles[0]["title"];
-                description = articles[0]["description"];
-                urlToImage = articles[0]["urlToImage"];
+                // title = articles[0]["title"];
+                // description = articles[0]["description"];
+                // urlToImage = articles[0]["urlToImage"];
 
-                title2 = articles[1]["title"];
-                description2 = articles[1]["description"];
-                urlToImage2 = articles[1]["urlToImage"];
+                // title2 = articles[1]["title"];
+                // description2 = articles[1]["description"];
+                // urlToImage2 = articles[1]["urlToImage"];
 
-                for (var i = 0; i < 5; i++) {
-                  titles.insert(i,articles[i]["title"]);
-                  descriptions.insert(i,articles[i]["description"]);
-                  urlToImages.insert(i,articles[i]["urlToImage"]);
-
+                for (var i = 0; i < 10; i++) {
+                  titles.insert(i, articles[i]["title"]);
+                  descriptions.insert(i, articles[i]["description"]);
+                  urlToImages.insert(i, articles[i]["urlToImage"]);
                 }
                 // titles.insert(0,articles[0]["title"]);
 
@@ -94,12 +106,18 @@ class _NewsScreenState extends State<NewsScreen> {
                 });
               },
             ),
+            Padding(
+              padding: EdgeInsets.all(10.0),
+            ),
             Image.network(urlToImages[0]),
             Card(
               child: ListTile(
                 title: Text(titles[0]),
                 subtitle: Text(descriptions[0]),
               ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10.0),
             ),
             Image.network(urlToImages[1]),
             Card(
@@ -108,11 +126,84 @@ class _NewsScreenState extends State<NewsScreen> {
                 subtitle: Text(descriptions[1]),
               ),
             ),
+            Padding(
+              padding: EdgeInsets.all(10.0),
+            ),
             Image.network(urlToImages[2]),
             Card(
               child: ListTile(
                 title: Text(titles[2]),
+                subtitle: Text(descriptions[2]),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10.0),
+            ),
+            Image.network(urlToImages[3]),
+            Card(
+              child: ListTile(
+                title: Text(titles[3]),
                 subtitle: Text(descriptions[3]),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10.0),
+            ),
+            Image.network(urlToImages[4]),
+            Card(
+              child: ListTile(
+                title: Text(titles[4]),
+                subtitle: Text(descriptions[4]),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10.0),
+            ),
+            Image.network(urlToImages[5]),
+            Card(
+              child: ListTile(
+                title: Text(titles[5]),
+                subtitle: Text(descriptions[5]),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10.0),
+            ),
+            Image.network(urlToImages[6]),
+            Card(
+              child: ListTile(
+                title: Text(titles[6]),
+                subtitle: Text(descriptions[6]),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10.0),
+            ),
+            Image.network(urlToImages[7]),
+            Card(
+              child: ListTile(
+                title: Text(titles[7]),
+                subtitle: Text(descriptions[7]),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10.0),
+            ),
+            Image.network(urlToImages[8]),
+            Card(
+              child: ListTile(
+                title: Text(titles[8]),
+                subtitle: Text(descriptions[8]),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10.0),
+            ),
+            Image.network(urlToImages[9]),
+            Card(
+              child: ListTile(
+                title: Text(titles[9]),
+                subtitle: Text(descriptions[9]),
               ),
             ),
           ],
