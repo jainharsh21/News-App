74ed311ee9188104a8ac152826b843d7e4d2de9c
HARSH JAIN <jainharsh270@gmail.com>
2019-10-24 14:44:02 +0530

    changes in getting data method

--- lib/ui/news_screen.dart
+++ lib/ui/news_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:news_app/utils/util.dart' as util;
 
-
 class NewsScreen extends StatefulWidget {
   String topicName;
   NewsScreen(this.topicName);
@@ -12,13 +11,14 @@ class NewsScreen extends StatefulWidget {
 }
 
 class _NewsScreenState extends State<NewsScreen> {
-  String apiId = '951b6fe097884996b05c2bd2cd3f8d44';
+  Map news;
+  List articles;
   String topicName;
   _NewsScreenState(this.topicName);
 
   void showData() async {
-    Map news  = await getNews(util.apiId, util.defaultNews);
-    List articles = news["articles"];
+    news = await getNews(util.apiId, topicName.toLowerCase());
+    articles = news["articles"];
     // print(news.toString());
     print(articles);
   }
@@ -33,24 +33,25 @@ class _NewsScreenState extends State<NewsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("$topicName",style: TextStyle(
-          fontFamily: 'Billabong',
-          fontSize: 35,
-          fontWeight: FontWeight.bold,
-        ),),
+        title: Text(
+          "$topicName",
+          style: TextStyle(
+            fontFamily: 'Billabong',
+            fontSize: 35,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
         centerTitle: true,
         backgroundColor: Colors.black,
       ),
     );
   }
 
-  Future<Map> getNews(String apiId,String topicName) async{
-    String apiUrl = "https://newsapi.org/v2/top-headlines?country=in&category=$topicName&apiKey=$apiId";
+  Future<Map> getNews(String apiId, String topicName) async {
+    String apiUrl =
+        "https://newsapi.org/v2/top-headlines?country=in&category=$topicName&apiKey=$apiId";
     http.Response response = await http.get(apiUrl);
 
     return json.decode(response.body);
   }
-
-
-
 }
